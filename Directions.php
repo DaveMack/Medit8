82b6a929bbb645914d69b33954a96a6841c99986
Tenmujiin <lgall19@gmail.com>
2016-10-10 10:36:37 +1000

    Split directions and login testing branches

--- Directions.php
+++ Directions.php
@@ -16,20 +16,7 @@
 
 <article style="position: fixed;"><p>Our school is located at 1310 Seventeen Mile Road, Seventeen Mile QLD 4344, on the edge of the 
 Lockyer National Park. This out-of-the-way location is perfect for those wanting a calm and relaxing location to meditate.</p>
-<div id="map" style="width:400px;height:400px;background:yellow"></div>
-
-<script>
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-    center: new google.maps.LatLng(-27.465158, 152.187860), zoom: 10
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-}
-</script>
-
-<script src="https://maps.googleapis.com/maps/api/js?callback=myMap"></script>
-
+<img src="./images/map.jpg" alt="" />
 
 </article>
 
