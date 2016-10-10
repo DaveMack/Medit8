4137c5f1df94e1041020ef44efa6dfe5feffbcfa
Tenmujiin <lgall19@gmail.com>
2016-10-10 11:01:41 +1000

    Interactive map working. Needs to be changed to use google directions instead of maps.

--- Directions.php
+++ Directions.php
@@ -15,7 +15,21 @@
 <!--Page contents-->
 <article style="position: fixed;"><p>Our school is located at 1310 Seventeen Mile Road, Seventeen Mile QLD 4344, on the edge of the 
 Lockyer National Park. This out-of-the-way location is perfect for those wanting a calm and relaxing location to meditate.</p>
-<img style="float:left; margin:20px; top:10px; width: 80%; height:auto "src="images/Map.jpg" alt="A map with the location of our school." />
+<div id="map" style="width:400px;height:400px;background:yellow"></div>
+
+<script>
+	var map;
+	function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), 
+		{
+		center: {lat: -27.465158, lng: 152.187860},
+		zoom: 8
+		});
+    }
+</script>
+
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCQa7GoSNe9XaK1mxjG8CHzUhARgHNwPug&callback=initMap"></script>
+
 </article>
 
 </body>
